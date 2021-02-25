404daeb1175d48beb9025ffa6f2a09b39cc12b52
tks2807 <57905569+tks2807@users.noreply.github.com>
2021-02-25 16:42:26 +0600

    Update server.go

--- server/server.go
+++ server/server.go
@@ -15,7 +15,21 @@ type Server struct {
 }
 
 func (s *Server) PrimeNumberDecomposition(ctx context.Context, req *pb.PrimeRequest) (*pb.PrimeReply, error) {
-
+	input := in.N1
+	var ans []int
+	for ;input%2==0;input=input/2 {
+		fmt.Print(2, " ")
+		ans = append(ans, 2)
+	}
+	i := 3
+	for ;i<=int(math.Sqrt(float64(n))); i = i + 2 {
+		for ;input%i==0; {
+			fmt.Print(i, " ")
+			ans = append(ans, i)
+			input = input/i
+		}
+	}
+	&pb.PrimeReply{N1: ans}, nil
 }
 
 func (s *Server) ComputeAverage(ctx context.Context, req *pb.AverageRequest) (*pb.AverageReply, error) {
